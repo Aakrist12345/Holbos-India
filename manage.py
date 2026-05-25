9e88dbe6aa9b03ffbd211103d563666720b0cd23
Akrist12345 <aakristsharma135@gmail.com>
2026-05-25 15:14:13 +0530

    Final Render deployment fix

--- manage.py
+++ manage.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
-from pathlib import Path
-
 
 def main():
-    BASE_DIR = Path(__file__).resolve().parent
-    INNER_DIR = BASE_DIR / "holbos_attendance"
-
-    sys.path.insert(0, str(INNER_DIR))
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "holbos_attendance.holbos_project.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holbos_project.settings')
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
