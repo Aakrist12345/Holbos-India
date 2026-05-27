8d536ceea9556b8d2586f5c73e01fd0c5f1e40a1
Akrist12345 <aakristsharma135@gmail.com>
2026-05-28 01:11:38 +0530

    Update settings for Render deployment

--- holbos_attendance/holbos_project/settings.py
+++ holbos_attendance/holbos_project/settings.py
@@ -43,7 +43,7 @@ if DEBUG:
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1",
     "http://localhost",
-    "https://holbos-india-1.onrender.com",
+    "https://holbos-attendance-erp.onrender.com",
 ]
 
 if os.environ.get('RENDER'):
