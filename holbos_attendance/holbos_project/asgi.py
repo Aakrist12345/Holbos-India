f568d40bfcc87c26297d599bf45f7eac312e7266
Akrist12345 <aakristsharma135@gmail.com>
2026-05-28 00:26:13 +0530

    Restore correct Django settings path

--- holbos_attendance/holbos_project/asgi.py
+++ holbos_attendance/holbos_project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holbos_attendance.holbos_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holbos_project.settings')
 
 application = get_asgi_application()
--- holbos_attendance/holbos_project/wsgi.py
+++ holbos_attendance/holbos_project/wsgi.py
@@ -17,6 +17,4 @@ sys.path.insert(0, str(PROJECT_ROOT))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holbos_attendance.holbos_project.settings')
-
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holbos_project.settings')
