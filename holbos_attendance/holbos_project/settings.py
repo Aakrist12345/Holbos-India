cab3dbf94fc867dbbd3d866ecde71b1761180a73
Akrist12345 <aakristsharma135@gmail.com>
2026-05-28 15:48:42 +0530

    Fix email backend timeout on Render

--- holbos_attendance/holbos_project/settings.py
+++ holbos_attendance/holbos_project/settings.py
@@ -151,7 +151,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 COMPENSATION_EMAIL_ENABLED = os.getenv('COMPENSATION_EMAIL_ENABLED', 'True').lower() == 'true'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = os.environ.get(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.console.EmailBackend"
+)
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
