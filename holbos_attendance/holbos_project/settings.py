8ee57485d62d076454dc2477531fa47a65c611fa
Akrist12345 <aakristsharma135@gmail.com>
2026-05-29 12:20:12 +0530

    Fix email configuration

--- holbos_attendance/holbos_project/settings.py
+++ holbos_attendance/holbos_project/settings.py
@@ -151,17 +151,22 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 COMPENSATION_EMAIL_ENABLED = os.getenv('COMPENSATION_EMAIL_ENABLED', 'True').lower() == 'true'
-EMAIL_BACKEND = os.environ.get(
+EMAIL_BACKEND = config(
     "EMAIL_BACKEND",
-    "django.core.mail.backends.smtp.EmailBackend"
+    default="django.core.mail.backends.smtp.EmailBackend"
 )
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+
+DEFAULT_FROM_EMAIL = config(
+    "DEFAULT_FROM_EMAIL",
+    default=EMAIL_HOST_USER
+)
 COMPENSATION_NOTIFICATION_EMAIL = config(
     "COMPENSATION_NOTIFICATION_EMAIL",
     default="holbosindia@gmail.com"
